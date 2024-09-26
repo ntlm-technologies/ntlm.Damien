4f2a9492c7b47aabf224a1117fe9cf11c4a837c4
Nicolas Taret <mambojoel@gmail.com>
2024-09-26 22:38:24 +0200

    Add Win project

---
+++ ntlm.Damien/ntlm.Damien.Win/Main.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace ntlm.Damien.Win
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            SuspendLayout();
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Main";
+            Text = "Ntlm Github Cloner";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ ntlm.Damien/ntlm.Damien.Win/Main.cs
@@ -0,0 +1,10 @@
+namespace ntlm.Damien.Win
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
